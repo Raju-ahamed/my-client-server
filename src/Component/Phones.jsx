52ec856affef4afe478ca34e6ffeda4b6d5e14a6
Raju-ahamed <rajuahamed7320@gmail.com>
2026-06-20 12:33:32 +0600

    fiels

---
+++ src/Component/Phones.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link, useLoaderData } from 'react-router';
+
+const Phones = () => {
+    const phones = useLoaderData();
+    return (
+        <div>
+            <h1> all phone here : {phones.length}</h1>
+            {
+                phones.map(phone => <li
+                    key={phone.id}
+
+                >
+                    <Link
+                        to={`/phone/${phone.id}`}>{phone.name}</Link>
+                </li>)
+            }
+
+        </div>
+    );
+};
+
+export default Phones;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,27 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Main from './Component/Main.jsx';
+import Phones from './Component/Phones.jsx';
+import Phone from './Component/Phone.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Main />,
+    children: [
+
+      {
+        path: "/phones",
+        element: <Phones></Phones>,
+        loader: () => fetch("http://localhost:4000/phone")
+      },
+      {
+        path: "/phone/:id",
+        element: <Phone></Phone>,
+        loader: ({ params }) => fetch(`http://localhost:4000/phone/${params.id}`)
+      }
+    ],
   },
 ]);
 
